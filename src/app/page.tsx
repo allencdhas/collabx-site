f4ad96510b6d66a7fe97507961f167d76edc38b7
Allen Chrisso Dhas <allencdvja@gmail.com>
2024-09-25 10:51:03 +0530

    theme and logo update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,36 @@
 import Image from "next/image";
 import { Inter } from 'next/font/google'
+import image from '../app/image.png';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      <div className="mx-10 flex flex-col w-[100%] lg:w-[60%] inter.classname">
-            <p className="pt-20 text-[9rem] font-bold ">
-              CollabX
+    
+    <div className="mx-10">
+      <div className="flex justify-center flex-row">
+        <div className="mt-20 flex flex-col items-center w-[100%] lg:w-[60%] inter.classname">
+            <p className="pt-20 text-[6rem] font-semibold ">
+              Collab<span className="text-purple-800">X</span>
             </p>
-
-            <p className="sm:w-[50%] pt-15 pb-10 pl-2">
+            <p className="my-3 sm:w-[70%] text-center text-4xl font-medium text-salte-400 dark:text-gray-50 ">
               A collaborative business platform for creators with limited resources to upscale your business.
+
             </p>
-            
+            {/* <p className="sm:w-[50%] my-6 text-center text-lg leading-6 text-salte-200 dark:text-gray-200">
+            A collaborative business platform for creators with limited resources to upscale your business.
+            </p> */}
+            <div className="mb-8 my-4 flex"><a href="https://github.com/ibelick/background-snippets" target="_blank" rel="noopener noreferrer" className="inline-flex"><span className="relative inline-block overflow-hidden rounded-full p-[1px]"><span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#a9a9a9_0%,#0c0c0c_50%,#a9a9a9_100%)] dark:bg-[conic-gradient(from_90deg_at_50%_50%,#171717_0%,#737373_50%,#171717_100%)]"></span><div className="inline-flex h-full w-full cursor-pointer justify-center rounded-full bg-black px-3 py-1 text-xs font-medium leading-5 text-slate-200 backdrop-blur-xl dark:bg-black dark:text-slate-100">Github<span className="inline-flex items-center pl-2 text-white dark:text-white">Here <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="pl-0.5 text-neutral dark:text-white"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg></span></div></span></a></div>
+
             <div className="pl-2">
               <button
-                className="bg-white text-black py-2 px-10  rounded-full w-fit"
+                className="bg-black border-3 border-purple-800 text-zinc-400 py-2 px-10 font-bold  rounded-full w-fit hover:bg-purple-900 hover:text-white"
               >
                 Launch dApp
               </button>
             </div>
-            
-          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,24 +4,26 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
+import logo from '../app/image.png';
+
 const NavBar = () => {
 
-
     return(
-        <div className='px-10 fixed w-full flex justify-between sm:py-2 bg-black sm:bg-transparent'>
-            <Image src='/favicon.ico' height={60} width={60} alt='img' />
-            <div className='sm:flex justify-evenly items-center gap-10 text-sm hidden'>
-                <div>
-                    <a href="#">Home</a>
-                </div>
-                <div>
-                    <a href="#">About</a>
-                </div>
-                <div>
-                    <a href="#">Contact</a>
+        <main>
+            <div className='px-20 fixed w-full flex justify-between py-2 sm:bg-transparent'>
+                <Image src={logo} height={100} width={100} alt='img'/>
+                <div className='flex justify-evenly items-center gap-10'>
+                    <div>
+                        <a href="#">Home</a>
+                    </div>
+
+                    <div>
+                        <a href="#">Contact</a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </main>
+        
     )
 }
 
